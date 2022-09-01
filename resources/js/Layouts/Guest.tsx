effd39c789d1f7a304496b0496db3ebe1bcaa516
Alejandro Randon <58668589+alerandon@users.noreply.github.com>
2022-09-01 17:53:46 -0400

    added interfaces and fixed tailwind config

--- resources/js/Layouts/Guest.tsx
+++ resources/js/Layouts/Guest.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/inertia-react';
 
-export default function Guest({ children }) {
+interface GuestProps {
+  children: React.ReactNode;
+}
+
+export default function Guest({ children }: GuestProps) {
   return (
     <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
       <div>
